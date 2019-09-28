adf7a3977cdd373ca9e18c695473b701df73d259
Michael Eskarous <mneskarous@gmail.com>
2019-09-28 13:55:35 -0700

    Changed controller function to match router endpoint

--- server/controller.js
+++ server/controller.js
@@ -7,21 +7,21 @@ const {
 
 const controller = {
 
-  // get: (req, res) => {
-  //   let { id } = req.params;
-  //   console.log('what is id', id)
-  //   getById(id)
-  //   .then(data => res.status(200).send(data))
-  //   .catch(err => res.status(400).send(err));
-  // },
-
   get: (req, res) => {
     let { id } = req.params;
     console.log('what is id', id)
-    getById(Math.floor(Math.random() * (Math.floor(10000000) - Math.ceil(9000001) + 1)) + Math.ceil(9000001))
+    getById(id)
     .then(data => res.status(200).send(data))
     .catch(err => res.status(400).send(err));
   },
+
+  // get: (req, res) => {
+  //   let { id } = req.params;
+  //   console.log('what is id', id)
+  //   getById(Math.floor(Math.random() * (Math.floor(10000000) - Math.ceil(9000001) + 1)) + Math.ceil(9000001))
+  //   .then(data => res.status(200).send(data))
+  //   .catch(err => res.status(400).send(err));
+  // },
 
   post: (req, res) => {
     let { product_category, product_name, price, brand_name, online_exclusive, review_star_count, review_count, colors, color_images, fit, size_standard, size_petite, size_plus, sizes_unavailable, size_petite_unavailable, size_plus_unavailable, image } = req.body;
